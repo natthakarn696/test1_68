f88221927ddc6d5ac78f9b69716f68e284d61876
natthakarn696 <natthakarnwong2008@gmail.com>
2025-06-30 10:03:18 +0700

    main

--- job3_10n72.c
+++ job3_10n72.c
@@ -4,7 +4,7 @@ void main()
     int num, num1=0, num2=0;
     printf("Enter number : ");
     scanf("%d", &num);
-    num1=num;
     num2=num*2;
-    printf("Double of the number is: %d\n", num2);
+    if(num2=num*2)
+    printf("num2 = %d", num2);
 }
